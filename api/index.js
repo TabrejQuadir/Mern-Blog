a44e10c9d1b2f4a0723b8ad2cf6b34fab244687e
Tabrej Quadir <tabrezquadir6@gmail.com>
2024-04-05 21:43:50 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,23 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from "./routes/user.route.js"
-
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO)
-.then(() => {
-    console.log("Connected")
-}).catch(() => {
-    console.log("Not connected")
-})
+.then(() => {console.log("Connected")})
+.catch(() => { console.log("Not connected")})
 
-app.use("/api/user", userRoutes)
+app.listen("4000", () => {console.log("Listening on port 400")})
 
-app.listen("4000", () => {
-    console.log("Listening on port 400")
-})
+app.use("/api/user", userRoutes);
+app.use('/api/auth', authRoutes);
 
 // mongodb+srv://tabrez:<password>@blog.akxexyx.mongodb.net/
