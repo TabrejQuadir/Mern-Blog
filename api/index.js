de5a03c5b0092052ee4edf8c60ea8abc70754005
Tabrej Quadir <tabrezquadir6@gmail.com>
2024-04-05 20:18:30 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.listen("4000",()=>{
-console.log("Listening on port 400")
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log("Connected")
+}).catch(() => {
+    console.log("Not connected")
 })
+
+app.listen("4000", () => {
+    console.log("Listening on port 400")
+})
+
+// mongodb+srv://tabrez:<password>@blog.akxexyx.mongodb.net/
