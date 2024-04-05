b83233c971b80af87fd01e5d558fb2f83c22965d
Tabrej Quadir <tabrezquadir6@gmail.com>
2024-04-05 22:08:19 +0530

    add a middleware and add a function to handle errors

--- api/index.js
+++ api/index.js
@@ -17,4 +17,14 @@ app.listen("4000", () => {console.log("Listening on port 400")})
 app.use("/api/user", userRoutes);
 app.use('/api/auth', authRoutes);
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
+
 // mongodb+srv://tabrez:<password>@blog.akxexyx.mongodb.net/
