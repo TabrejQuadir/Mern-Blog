73c150b813e65b9ddd8cf158e1046a5bdd3e7f2e
Tabrej Quadir <tabrezquadir6@gmail.com>
2024-04-05 17:49:16 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
 import './App.css'
+import Aboutt from './pages/Aboutt'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from "./pages/Projects"
+
+
+
 
 function App() {
 
   return (
-    <>
-    <h1 className='text-red-500 text-3xl'>Hello</h1>
-    </>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<Aboutt/>}/>
+    <Route path='/sign-in' element={<SignIn/>}/>
+    <Route path='/sign-up' element={<SignUp/>}/>
+    <Route path='/dashboard' element={<Dashboard/>}/>
+    <Route path='/projects' element={<Projects/>}/>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Aboutt.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Aboutt = () => {
+  return (
+    <div className='text-3xl text-red-500'>Aboutt</div>
+  )
+}
+
+export default Aboutt
