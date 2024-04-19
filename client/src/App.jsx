3075183cf224c91310f7666cfe30b7b61bfdb1e0
Tabrej Quadir <tabrezquadir6@gmail.com>
2024-04-19 20:43:40 +0530

    make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Aboutt from './pages/Aboutt'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
@@ -8,26 +8,27 @@ import Dashboard from './pages/Dashboard'
 import Projects from "./pages/Projects"
 import Header from './components/Header'
 import Footer from './components/Footer';
-
-
+import PrivateRoute from './components/PrivateRoute';
 
 
 function App() {
 
   return (
-   <BrowserRouter>
-   <Header/>
-   <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/about' element={<Aboutt/>}/>
-    <Route path='/sign-in' element={<SignIn/>}/>
-    <Route path='/sign-up' element={<SignUp/>}/>
-    <Route path='/dashboard' element={<Dashboard/>}/>
-    <Route path='/project' element={<Projects/>}/>
-   </Routes>
-   <Footer />
-   </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<Aboutt />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route path='/project' element={<Projects />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
