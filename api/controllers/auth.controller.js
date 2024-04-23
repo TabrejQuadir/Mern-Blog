3dc67c3bd0d9354c4ed3ce4764c605ec1258e80d
Tabrej Quadir <tabrezquadir6@gmail.com>
2024-04-23 19:59:01 +0530

    adding upadte user api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -50,6 +50,7 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, 'Invalid password'));
     }
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    console.log(process.env.JWT_SECRET);
 
     const { password: pass, ...rest } = validUser._doc;
 
