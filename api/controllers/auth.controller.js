602f7c65cfa48c43f6208cce3581f1fc3ffcd2d9
Tabrej Quadir <tabrezquadir6@gmail.com>
2024-04-28 19:55:41 +0530

    add admin fucntionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,33 +4,33 @@ import { errorHandler } from '../utilis/error.js';
 import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    if (
-        !username ||
-        !email ||
-        !password ||
-        username === '' ||
-        email === '' ||
-        password === ''
-      ) {
-        next(errorHandler(400, 'All fields are required'));
-      }
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400, 'All fields are required'));
+  }
 
-      const hashedPassword = bcryptjs.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-      const newUser = new User({
-        username,
-        email,
-        password: hashedPassword,
-      });
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
-      try {
-        await newUser.save();
-        res.json('Signup successful');
-      } catch (error) {
-        next(error)
-      }
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const signin = async (req, res, next) => {
@@ -39,7 +39,7 @@ export const signin = async (req, res, next) => {
   if (!email || !password || email === '' || password === '') {
     next(errorHandler(400, 'All fields are required'));
   }
-  
+
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
@@ -49,14 +49,15 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(400, 'Invalid password'));
     }
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    console.log(process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET);
 
     const { password: pass, ...rest } = validUser._doc;
 
     res.status(200).cookie('access_token', token, {
-        httpOnly: true,
-      }).json(rest);
+      httpOnly: true,
+    }).json(rest);
   } catch (error) {
     next(error);
   }
@@ -67,7 +68,10 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = user._doc;
       res
         .status(200)
@@ -89,7 +93,10 @@ export const google = async (req, res, next) => {
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
