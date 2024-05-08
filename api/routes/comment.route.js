a6ae00c83b903b29f7b1ac25d6ac5528f7bf7850
Tabrej Quadir <tabrezquadir6@gmail.com>
2024-05-08 18:22:09 +0530

    show the comments of a post

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utilis/verifyUser.js';
-import { createComment } from '../controllers/comment.controller.js';
+import { createComment, getPostComments } from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
 
 router.post('/create', verifyToken, createComment);
-
+router.get('/getPostComments/:postId', getPostComments);
 export default router;
