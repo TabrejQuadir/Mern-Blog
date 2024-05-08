0b737a172ce284b91fc31559e53a670e83387e64
Tabrej Quadir <tabrezquadir6@gmail.com>
2024-05-08 17:32:03 +0530

    add comment section to the post page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from "cookie-parser";
 // import multer from "multer";
 import cors from "cors";
@@ -56,6 +57,7 @@ app.listen("4000", () => {console.log("Listening on port 4000")})
 app.use("/api/user", userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
