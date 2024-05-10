b35dc8346c5f4efff7fe7bc5147f7dbaa1996719
Tabrej Quadir <tabrezquadir6@gmail.com>
2024-05-10 15:39:27 +0530

    add comments section to the admin dashboard

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utilis/verifyUser.js';
-import { createComment, getPostComments, likeComment, editComment, deleteComment } from '../controllers/comment.controller.js';
+import { createComment, getPostComments, likeComment, editComment, deleteComment, getcomments } from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
@@ -10,4 +10,5 @@ router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
 router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments', verifyToken, getcomments);
 export default router;
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,11 @@
 import { Sidebar } from 'flowbite-react';
-import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup } from 'react-icons/hi';
-import { useEffect, useState } from 'react';
+import {
+  HiUser,
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiAnnotation,
+} from 'react-icons/hi';import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
@@ -62,6 +67,7 @@ export default function DashSidebar() {
             </Link>
           )}
           {currentUser.isAdmin && (
+            <>
             <Link to='/dashboard?tab=users'>
               <Sidebar.Item
                 active={tab === 'users'}
@@ -71,6 +77,16 @@ export default function DashSidebar() {
                 Users
               </Sidebar.Item>
             </Link>
+            <Link to='/dashboard?tab=comments'>
+              <Sidebar.Item
+                active={tab === 'comments'}
+                icon={HiAnnotation}
+                as='div'
+              >
+                Comments
+              </Sidebar.Item>
+            </Link>
+          </>
           )}
           <Sidebar.Item
             icon={HiArrowSmRight}
