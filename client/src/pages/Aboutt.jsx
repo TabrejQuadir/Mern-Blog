301e12940ce98f2caf13cbe93f2d3eb5dc7e1602
Tabrej Quadir <tabrezquadir6@gmail.com>
2024-05-10 21:39:38 +0530

    complete about and projects pages

--- client/src/pages/Aboutt.jsx
+++ client/src/pages/Aboutt.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 const Aboutt = () => {
   return (
-    <div className='text-3xl text-red-500'>Aboutt</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+    <div className='max-w-2xl mx-auto p-3 text-center'>
+      <div>
+        <h1 className='text-3xl font font-semibold text-center my-7'>
+          About <span className='bg-gradient-to-r from-yellow-500 via-red-500 to-pink-500 text-transparent bg-clip-text gradient-text  dark:bg-gradient-to-l dark:from-cyan-400  dark:to-yellow-300 dark:text-transparent dark:bg-clip-text'>Tabrej' Blog</span>
+        </h1>
+        <div className='text-md text-gray-500 flex flex-col gap-6 '>
+          <p>
+            Welcome to Tabrej's Blog! This blog was created by Tabrej Quadir
+            as a personal project to share his thoughts and ideas with the
+            world. Tabrej is a passionate developer who loves to write about
+            technology, coding, and everything in between.
+          </p>
+
+          <p>
+            On this blog, you'll find weekly articles and tutorials on topics
+            such as web development, software engineering, and programming
+            languages. Tabrej is always learning and exploring new
+            technologies, so be sure to check back often for new content!
+          </p>
+
+          <p>
+            We encourage you to leave comments on our posts and engage with
+            other readers. You can like other people's comments and reply to
+            them as well. We believe that a community of learners can help
+            each other grow and improve.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 }
+  
 
 export default Aboutt
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import CallToAction from "../components/CallToAction"
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Pojects</h1>
+      <p className='text-md text-gray-500'>Build fun and engaging projects while learning
+        <span className='ml-1 font-bold bg-gradient-to-r from-yellow-500 via-red-500 to-pink-500 text-transparent bg-clip-text gradient-text  dark:bg-gradient-to-l dark:from-cyan-400  dark:to-yellow-300 dark:text-transparent dark:bg-clip-text'>
+         HTML, CSS, and JavaScript!
+        </span></p>
+      <CallToAction />
+    </div>
   )
 }
 
